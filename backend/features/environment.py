4b1324506d324960c68a40126bdd61bf33178f6a
Garrett Woodson <garrettwoodson@icloud.com>
2025-02-25 21:39:59 -0500

    fix profile bug

--- backend/features/environment.py
+++ backend/features/environment.py
@@ -10,6 +10,15 @@ def before_feature(context, feature):
     context.app = app
     context.client = context.app.test_client()
 
+    with context.app.app_context():
+        db = get_db()
+        # Either delete existing rows:
+        db.execute("DELETE FROM users")
+        db.execute("INSERT INTO users (username, email, password, name, phoneNumber) VALUES (?, ?, ?, ?, ?)",
+            ('admin', 'adming@gmail.com', hashed_password, 'Admin', '1234567890')
+            )
+        db.commit()
+
 def before_scenario(context, scenario):
     """
     This hook runs before *each* scenario and resets the `users` table
@@ -25,4 +34,9 @@ def before_scenario(context, scenario):
         db.execute("INSERT INTO users (username, email, password, name, phoneNumber) VALUES (?, ?, ?, ?, ?)",
             ('admin', 'adming@gmail.com', hashed_password, 'Admin', '1234567890')
             )
+        db.execute("INSERT INTO users (username, email, password, name, phoneNumber) VALUES (?, ?, ?, ?, ?)",
+            ('other', 'other@gmail.com', hashed_password, 'other', '1234567891'))
+
+        db.execute("INSERT INTO users (username, email, password, name, phoneNumber) VALUES (?, ?, ?, ?, ?)",
+            ('empty', 'empty@gmail.com', hashed_password, 'empty', '1234567892'))
         db.commit()
